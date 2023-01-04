8df51bfeec7c040e5c01820b13ff4dd6662101bf
Oliver Tušla <oliver.tusla@seznam.cz>
2023-01-04 18:48:28 +0100

    test: fix missing parentheses in test output

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -23,12 +23,12 @@ val tests = List(
   (
     testInput1,
     "List(ListItem(List(IdentifierItem(define), ListItem(List(IdentifierItem(null?), IdentifierItem(x))), ListItem(List(IdentifierItem(=), IdentifierItem(x), IdentifierItem(nil))))), ListItem(List(IdentifierItem(define), ListItem(List(IdentifierItem(my-append), IdentifierItem(it), IdentifierItem(lst))), ListItem(List(IdentifierItem(if), ListItem(List(IdentifierItem(null?), IdentifierItem(lst))), ListItem(List(IdentifierItem(cons), IdentifierItem(it), IdentifierItem(nil))), ListItem(List(IdentifierItem(let), ListItem(List(IdentifierItem(left))), ListItem(List(ListItem(List(IdentifierItem(car), IdentifierItem(lst))))), ListItem(List(IdentifierItem(cons), IdentifierItem(left), ListItem(List(IdentifierItem(my-append), IdentifierItem(it), ListItem(List(IdentifierItem(cdr), IdentifierItem(lst))))))))))))))",
-    "List(LDF(List(NIL, LD((0,0)), EQ(), RTN())), DEF(), LDF(List(NIL, LD((0,1)), CONS(), LD((1,0)), AP(), SEL(List(NIL, LD((0,0)), CONS(), JOIN()),List(NIL, LD((0,1)), CAR(), CONS(), LDF(List(NIL, LD((1,1)), CDR(), CONS(), LD((1,0)), CONS(), LD((2,1)), AP(), LD((0,0)), CONS(), RTN())), AP(), JOIN())), RTN())), DEF())"
+    "List(LDF(List(NIL(), LD((0,0)), EQ(), RTN())), DEF(), LDF(List(NIL(), LD((0,1)), CONS(), LD((1,0)), AP(), SEL(List(NIL(), LD((0,0)), CONS(), JOIN()),List(NIL(), LD((0,1)), CAR(), CONS(), LDF(List(NIL(), LD((1,1)), CDR(), CONS(), LD((1,0)), CONS(), LD((2,1)), AP(), LD((0,0)), CONS(), RTN())), AP(), JOIN())), RTN())), DEF())"
   ),
   (
     testInput2,
     "List(ListItem(List(IdentifierItem(define), ListItem(List(IdentifierItem(rec), IdentifierItem(x))), ListItem(List(IdentifierItem(rec), ListItem(List(IdentifierItem(+), IdentifierItem(x), IntItem(1))))))))",
-    "List(LDF(List(NIL, LDC(IntItem(1)), LD((0,0)), ADD(), CONS(), LD((1,0)), AP(), RTN())), DEF())"
+    "List(LDF(List(NIL(), LDC(IntItem(1)), LD((0,0)), ADD(), CONS(), LD((1,0)), AP(), RTN())), DEF())"
   )
 )
 
