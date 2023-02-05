99555cfa6a518ae109cf83b10476397a6f7883b6
Oliver Tušla <oliver.tusla@seznam.cz>
2023-02-05 16:34:24 +0100

    item: ConsItem apply

--- src/main/scala/util/Item.scala
+++ src/main/scala/util/Item.scala
@@ -5,6 +5,10 @@ trait Item
 
 case class ConsItem(value: (Item, Item)) extends Item
 
+object ConsItem {
+  def apply(i1: Item, i2: Item): ConsItem = ConsItem((i1, i2))
+}
+
 case class DoubleItem(value: Double) extends Item
 
 case class IdentifierItem(value: String) extends Item
