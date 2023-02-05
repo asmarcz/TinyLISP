69bbb2f9dc34110ad96d1187a0e291058ca68483
Oliver Tušla <oliver.tusla@seznam.cz>
2023-02-05 20:57:48 +0100

    main: read filename from command line and interpret it

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,29 @@
 package asmar.tinylisp
 
+import parser.*
+import secd.compiler.CompilationManager
+import secd.runtime.Runtime
+
+import scala.collection.mutable
+import scala.io.Source
+
 @main
-def main(): Unit = {
+def main(filename: String): Unit = {
+  val source = Source.fromFile(filename)
+  val code = source.mkString
+  source.close()
+
+  rep(whitespace(item()))(code) match
+    case Accept(value, rem) =>
+      if (rem.forall(_.isWhitespace)) {
+        val instructions = CompilationManager().compile(value)
+        val runtime = Runtime(
+          mutable.Stack.from(instructions),
+          exceptionHandler = (ex: Exception, _) => throw ex
+        )
+        runtime.run()
+        runtime.getResult.foreach(println)
+      }
+      else throw IllegalArgumentException(s"Unexpected input at the end: '$rem'")
+    case Reject(_) => throw IllegalArgumentException()
 }
